2c548f56e3fc6531d8d6c17688807a53493fa295
Alexandr Korsak <alex.korsak@gmail.com>
2020-11-26 11:52:21 +0100

    add metabase installation

--- deploy/ansible/files/gen-cert.sh
+++ deploy/ansible/files/gen-cert.sh
@@ -5,3 +5,19 @@ docker run -it --rm \
       certonly \
       --webroot --webroot-path=/data/letsencrypt \
       -d api.balconygames.com
+
+docker run -it --rm \
+      -v /opt/app/certs:/etc/letsencrypt \
+      -v /opt/app/certs-data:/data/letsencrypt \
+      deliverous/certbot \
+      certonly \
+      --webroot --webroot-path=/data/letsencrypt \
+      -d metabase.balconygames.com
+
+docker run -it --rm \
+      -v /opt/app/certs:/etc/letsencrypt \
+      -v /opt/app/certs-data:/data/letsencrypt \
+      deliverous/certbot \
+      certonly \
+      --webroot --webroot-path=/data/letsencrypt \
+      -d dashboard.balconygames.com
