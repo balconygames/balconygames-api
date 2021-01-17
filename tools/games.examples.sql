2fcb6c06c45835d45a021e0b4ce54f2778c325ca
Alexandr Korsak <alex.korsak@gmail.com>
2021-01-17 18:54:49 +0100

    add example of generating one more application

---
+++ tools/games.examples.sql
@@ -0,0 +1,11 @@
+CREATE EXTENSION "uuid-ossp";
+
+insert into games values ('87a4b609-b176-4289-991d-682703ab1889', 'Dice Blast');
+insert into apps values ('87a4b609-b176-4289-991d-682703ab1889', '111111111111111111111111111111111111', '0.0.1', 'EDITOR', 'EDITOR', 'MOBILE', true);
+insert into apps values ('87a4b609-b176-4289-991d-682703ab1889', uuid_generate_v4(), '0.0.1', 'ANDROID', 'AMAZON', 'MOBILE', true);
+insert into apps values ('87a4b609-b176-4289-991d-682703ab1889', uuid_generate_v4(), '0.0.1', 'ANDROID', 'GOOGLE_PLAY', 'MOBILE', true);
+insert into apps values ('87a4b609-b176-4289-991d-682703ab1889', uuid_generate_v4(), '0.0.1', 'ANDROID', 'HUAWEI', 'MOBILE', true);
+insert into apps values ('87a4b609-b176-4289-991d-682703ab1889', uuid_generate_v4(), '0.0.1', 'ANDROID', 'TAP_TAP', 'MOBILE', true);
+insert into apps values ('87a4b609-b176-4289-991d-682703ab1889', uuid_generate_v4(), '0.0.1', 'ANDROID', 'UDP', 'MOBILE', true);
+insert into apps values ('87a4b609-b176-4289-991d-682703ab1889', uuid_generate_v4(), '0.0.1', 'APPLE', 'APP_STORE', 'MOBILE', true);
+insert into apps values ('87a4b609-b176-4289-991d-682703ab1889', uuid_generate_v4(), '0.0.1', 'WEB', 'FACEBOOK', 'MOBILE', true);
